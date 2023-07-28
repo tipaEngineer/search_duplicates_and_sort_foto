ba8bd751460a6249a3a0c6b6a6815a634bf5ca69
tipaproger <lancer5610@gmail.com>
2023-07-28 13:53:19 +0300

    added information about completed sorting

--- main.py
+++ main.py
@@ -31,11 +31,22 @@ def sort_files_and_fotos():
 
 def copy_and_delete_files():
     sort_files=sort_files_and_fotos()
+    all_size = 0
+    for size in range(3, len(sort_files),4):
+        all_size = all_size + sort_files[size]
+        remain_size = all_size    
     for index in range(0,len(sort_files),4):
-        shutil.copy2(pathlib.Path(sort_files[index+1], sort_files[index]), pathlib.Path("E:/","sorted", datetime.datetime.fromtimestamp(sort_files[index+2]).strftime('%Y-%m-%d'), sort_files[index]))
+        try:
+            shutil.copy2(pathlib.Path(sort_files[index+1], sort_files[index]), pathlib.Path("E:/","sorted", datetime.datetime.fromtimestamp(sort_files[index+2]).strftime('%Y-%m-%d'), sort_files[index]))
+        except Exception:
+            import traceback
+            traceback.print_exc()
+            
         print('file ' + fspath(pathlib.Path(sort_files[index+1], sort_files[index])) + " copied")
         pathlib.Path(sort_files[index+1], sort_files[index]).unlink()
         print('file ' + fspath(pathlib.Path(sort_files[index+1], sort_files[index])) + ' deleted')
-    
+        remain_size = remain_size - sort_files[index+3]
+        print(str(round( (remain_size*100)/all_size    ,2)) + "% left")
+               
 if __name__ == '__main__':
     copy_and_delete_files()
